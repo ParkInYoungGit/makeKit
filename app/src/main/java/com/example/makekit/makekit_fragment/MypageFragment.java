2e7faea74ff366172f4aaebdfb8e278c0dda3f15
mg6613 <mg6613@naver.com>
2021-01-05 23:31:02 +0900

    구매내역, 판매내역, 찜, 후기, 회원정보수정 버튼 활성화

--- app/src/main/java/com/example/makekit/makekit_fragment/MypageFragment.java
+++ app/src/main/java/com/example/makekit/makekit_fragment/MypageFragment.java
@@ -21,7 +21,8 @@ import com.example.makekit.makekit_activity.UserModifyActivity;
 
 public class MypageFragment extends Fragment {
 
-    Button setting_btn, notice_btn, chatlist_btn;
+    Button setting_btn, buylist_btn, salelist_btn, likelist_btn, reviewlist_btn;  // 회원정보 수정, 구매내역, 판매내역, 찜목록, 후기목록
+
 
 
 
@@ -31,13 +32,26 @@ public class MypageFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_mypage, container, false);
 
         setting_btn = v.findViewById(R.id.setting_btn);
-        setting_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), UserModifyActivity.class);
-                startActivity(intent);
-            }
-        });
+        buylist_btn = v.findViewById(R.id.buylist_btn);
+        salelist_btn = v.findViewById(R.id.salelist_btn);
+        likelist_btn = v.findViewById(R.id.likelist_btn);
+        reviewlist_btn = v.findViewById(R.id.reviewlist_btn);
+
+        setting_btn.setOnClickListener(onClickListener);
+        buylist_btn.setOnClickListener(onClickListener);
+        salelist_btn.setOnClickListener(onClickListener);
+        likelist_btn.setOnClickListener(onClickListener);
+        reviewlist_btn.setOnClickListener(onClickListener);
+
+
+
+//        setting_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getContext(), UserModifyActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
         return v;
 //
@@ -76,5 +90,35 @@ public class MypageFragment extends Fragment {
 //
 
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.setting_btn:
+                    Intent intent = new Intent(getActivity(), UserModifyActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.buylist_btn:
+                    Intent intent1 = new Intent(getActivity(), BuyListActivity.class);
+                    startActivity(intent1);
+                    break;
+                case R.id.salelist_btn:
+                    Intent intent2 = new Intent(getActivity(), SaleListActivity.class);
+                    startActivity(intent2);
+                    break;
+                case R.id.likelist_btn:
+                    Intent intent3 = new Intent(getActivity(), LikeProductActivity.class);
+                    startActivity(intent3);
+                    break;
+                case R.id.reviewlist_btn:
+                    Intent intent4 = new Intent(getActivity(), ReviewListActivity.class);
+                    startActivity(intent4);
+                    break;
+            }
+
+        }
+    };
 
 }
