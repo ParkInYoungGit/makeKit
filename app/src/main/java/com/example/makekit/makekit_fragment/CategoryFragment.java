044e802e13310657a9a417b83d2ced40400c7b58
mg6613 <mg6613@naver.com>
2021-01-05 12:55:50 +0900

    layout

--- app/src/main/java/com/example/makekit/makekit_fragment/CategoryFragment.java
+++ app/src/main/java/com/example/makekit/makekit_fragment/CategoryFragment.java
@@ -21,9 +21,9 @@ public class CategoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Fragment는 Activity가 아니기때문에 리턴값과 레이아웃을 변수로 정해준다.
-        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_category, container, false);
 
-        listView = v.findViewById(R.id.lv_rec);
+//        listView = v.findViewById(R.id.lv_rec);
 
 
         return v;
