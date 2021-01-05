425d6011e54308d969c88f1b05b629749e5b507b
young <775973@daum.net>
2021-01-05 17:31:38 +0900

    no message

--- app/src/main/java/com/example/makekit/makekit_fragment/MypageFragment.java
+++ app/src/main/java/com/example/makekit/makekit_fragment/MypageFragment.java
@@ -1,22 +1,67 @@
 package com.example.makekit.makekit_fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.makekit.R;
+import com.example.makekit.makekit_activity.BuyListActivity;
+import com.example.makekit.makekit_activity.LikeProductActivity;
+import com.example.makekit.makekit_activity.ReviewListActivity;
+import com.example.makekit.makekit_activity.SaleListActivity;
 
 
 public class MypageFragment extends Fragment {
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Fragment는 Activity가 아니기때문에 리턴값과 레이아웃을 변수로 정해준다.
         View v = inflater.inflate(R.layout.fragment_mypage, container, false);
         return v;
+//
+//        v.findViewById(R.id.buylist_btn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(), BuyListActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        v.findViewById(R.id.salelist_btn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(), SaleListActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        v.findViewById(R.id.likelist_btn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(), LikeProductActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        v.findViewById(R.id.reviewlist_btn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(), ReviewListActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//
+
     }
+
 }
