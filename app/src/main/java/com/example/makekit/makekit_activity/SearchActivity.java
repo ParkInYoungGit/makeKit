34a4e78597a3dfa8b36f38e08565db002110424d
DESKTOP-RPFTG8R\admin <e994301s@gmail.com>
2021-01-06 14:13:45 +0900

    검색 기능 대략 완성

--- app/src/main/java/com/example/makekit/makekit_activity/SearchActivity.java
+++ app/src/main/java/com/example/makekit/makekit_activity/SearchActivity.java
@@ -6,18 +6,79 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.example.makekit.R;
+import com.example.makekit.makekit_asynctask.NetworkTask_DH;
+import com.example.makekit.makekit_bean.Product;
+import com.example.makekit.makekit_fragment.ProductAdapter;
+import com.example.makekit.makekit_fragment.SearchAdapter;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
+
+    String urlAddrBase = null;
+    String urlAddrGetData = null;
+    ArrayList<Product> products;
+    SearchAdapter adapter;
+    ListView listView;
+    String IP;
+    String email;
+    EditText search_EdT;
+    ImageView search_Iv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        listView = findViewById(R.id.searchListView);
+        search_EdT = findViewById(R.id.search_ET);
+        search_Iv = findViewById(R.id.search_Iv);
+
+        // ip와 email 받아오기
+        //
+        //
+        //
+        //
+        //
+
+        search_Iv.setOnClickListener(mClickListener);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
+    View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            connectGetData();
+
+        }
+    };
+
+    private void connectGetData() {
+        try {
+            urlAddrBase = "http://" + IP + ":8080/makeKit/Image/";
+            NetworkTask_DH networkTask = new NetworkTask_DH(SearchActivity.this, urlAddrGetData, "select");
+            Object obj = networkTask.execute().get();
+            products = (ArrayList<Product>) obj;
+            adapter = new SearchAdapter(SearchActivity.this, R.layout.search_layout, products, urlAddrBase); // 아댑터에 값을 넣어준다.
+            listView.setAdapter(adapter);  // 리스트뷰에 어탭터에 있는 값을 넣어준다.
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- app/src/main/java/com/example/makekit/makekit_asynctask/NetworkTask_DH.java
+++ app/src/main/java/com/example/makekit/makekit_asynctask/NetworkTask_DH.java
@@ -1,20 +1,143 @@
 package com.example.makekit.makekit_asynctask;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.widget.ProgressBar;
 
+import com.example.makekit.makekit_bean.Product;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class NetworkTask_DH extends AsyncTask<Integer, String, Object> {
 
     Context context = null;
     String mAddr = null;
-    ProgressBar progressBar = null;
-    ArrayList<>
+    ProgressDialog progressDialog = null;
+    ArrayList<Product> products;
+    String where = null;
+
+    public NetworkTask_DH(Context context, String mAddr, String where) {
+        this.context = context;
+        this.mAddr = mAddr;
+        this.products = new ArrayList<Product>();
+        this.where = where;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setTitle("Dialogue");
+        progressDialog.setMessage("Get ....");
+        progressDialog.show();
+    }
 
     @Override
     protected Object doInBackground(Integer... integers) {
-        return null;
+        StringBuffer stringBuffer = new StringBuffer();
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+
+        String result = null;
+
+
+        try {
+            URL url = new URL(mAddr);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setConnectTimeout(10000);
+
+            if(httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                inputStream = httpURLConnection.getInputStream();
+                inputStreamReader = new InputStreamReader(inputStream);
+                bufferedReader = new BufferedReader(inputStreamReader);
+
+                while (true){
+                    String strline = bufferedReader.readLine();
+                    if(strline == null) break;
+                    stringBuffer.append(strline + "\n");
+                }
+
+                if(where.equals("select")){
+                    parserSelect(stringBuffer.toString());
+                }else{
+                    result = parserAction(stringBuffer.toString());
+                }
+
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                if(bufferedReader != null) bufferedReader.close();
+                if(inputStreamReader != null) inputStreamReader.close();
+                if(inputStream != null) inputStream.close();
+
+            }catch (Exception e2){
+                e2.printStackTrace();
+            }
+        }
+
+        if(where.equals("select")){
+            return products;
+        }else{
+            return result;
+        }
+
     }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+        progressDialog.dismiss();
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+    }
+
+
+    private void parserSelect(String s){
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+//            JSONArray jsonArray = new JSONArray(jsonObject.getString("students_info"));
+//            products.clear();
+//            for(int i = 0; i < jsonArray.length(); i++){
+//                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+//                String code = jsonObject1.getString("code");
+//                String name = jsonObject1.getString("name");
+//                String dept = jsonObject1.getString("dept");
+//                String phone = jsonObject1.getString("phone");
+//
+////                Product product = new Product(code, name, dept, phone);
+////                products.add(product);
+//                // Log.v(TAG, member.toString());
+//            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private String parserAction(String s){
+        String returnValue = null;
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            returnValue = jsonObject.getString("result");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return returnValue;
+    }
+
+
 }
