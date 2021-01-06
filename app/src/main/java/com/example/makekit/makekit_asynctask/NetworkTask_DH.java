6419699fa63f3539531d143b7b36698761b3b336
DESKTOP-RPFTG8R\admin <e994301s@gmail.com>
2021-01-06 10:47:18 +0900

    product bean 수정

---
+++ app/src/main/java/com/example/makekit/makekit_asynctask/NetworkTask_DH.java
@@ -0,0 +1,20 @@
+package com.example.makekit.makekit_asynctask;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+
+public class NetworkTask_DH extends AsyncTask<Integer, String, Object> {
+
+    Context context = null;
+    String mAddr = null;
+    ProgressBar progressBar = null;
+    ArrayList<>
+
+    @Override
+    protected Object doInBackground(Integer... integers) {
+        return null;
+    }
+}
