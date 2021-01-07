e3d054bfab3ad3dffb0b0b30ac603a7824ab9f3a
mg6613 <mg6613@naver.com>
2021-01-07 21:09:09 +0900

    회원정보수정 주소, 수정, 21/1/7, 21:00, Min

--- app/src/main/java/com/example/makekit/makekit_activity/UserModifyActivity.java
+++ app/src/main/java/com/example/makekit/makekit_activity/UserModifyActivity.java
@@ -3,11 +3,14 @@ package com.example.makekit.makekit_activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -25,6 +28,9 @@ import com.example.makekit.makekit_bean.User;
 import java.util.ArrayList;
 
 public class UserModifyActivity extends AppCompatActivity {
+
+    private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
+
 
     final static String TAG = "First";
 
@@ -101,6 +107,20 @@ public class UserModifyActivity extends AppCompatActivity {
         update_btn = findViewById(R.id.user_update_btn);
         update_btn.setOnClickListener(onClickListener);
 
+        user_address = (EditText) findViewById(R.id.user_address);
+
+        Button btn_update_user = (Button) findViewById(R.id.userModiAddress_button);
+
+        if (btn_update_user != null) {
+            btn_update_user.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(UserModifyActivity.this, WebViewActivity.class);
+                    startActivityForResult(i, SEARCH_ADDRESS_ACTIVITY);
+                }
+            });
+        }
+
     }
 
     @Override
@@ -163,5 +183,37 @@ public class UserModifyActivity extends AppCompatActivity {
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+    // 이메일 주소 찾기
+    public void onActivityResult ( int requestCode, int resultCode, Intent intent)
+    {
+        super.onActivityResult(requestCode, resultCode, intent);
+        switch (requestCode) {
+            case SEARCH_ADDRESS_ACTIVITY:
+                if (resultCode == RESULT_OK) {
+                    String data = intent.getExtras().getString("data");
+                    if (data != null) {
+                        user_address.setText(data);
+                    }
+                }
+                break;
+        }
+    }
+
+    // 화면 touch 시 키보드 숨기기
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View focusView = getCurrentFocus();
+        if (focusView != null) {
+            Rect rect = new Rect();
+            focusView.getGlobalVisibleRect(rect);
+            int x = (int) ev.getX(), y = (int) ev.getY();
+            if (!rect.contains(x, y)) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                if (imm != null)
+                    imm.hideSoftInputFromWindow(focusView.getWindowToken(), 0);
+                focusView.clearFocus();
+            }
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
