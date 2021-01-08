6ebad5a355fbecdf7fb03573a1b300ed06a6094c
RiaSong <donna_s@sookmyung.ac.kr>
2021-01-08 19:29:36 +0900

    1/8 Ria_ 로그인

--- app/src/main/java/com/example/makekit/makekit_activity/LoginActivity.java
+++ app/src/main/java/com/example/makekit/makekit_activity/LoginActivity.java
@@ -1,18 +1,36 @@
 package com.example.makekit.makekit_activity;
 
+import android.content.Intent;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.makekit.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.kakao.sdk.auth.LoginClient;
 import com.kakao.sdk.auth.model.OAuthToken;
 import com.kakao.sdk.user.UserApiClient;
@@ -28,7 +46,14 @@ public class LoginActivity extends AppCompatActivity {
     private View btnLogin, btnLogout;
     private TextView nickName;
     private ImageView profileIMG;
+    ImageView login_google;
+    SignInButton google;
+    Button login_join_btn;
+    TextView non_members;
 
+    private FirebaseAuth mAuth = null;
+    private GoogleSignInClient mGoogleSignInClient;
+    public static final int RC_SIGN_IN = 1;
 
 
     @Override
@@ -41,7 +66,25 @@ public class LoginActivity extends AppCompatActivity {
         nickName = findViewById(R.id.nickname);
         profileIMG = findViewById(R.id.profile);
 
-        // 별도의 콜백 객체 선언
+        // 구글 로그인 세팅
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        login_google = findViewById(R.id.login_google);
+        login_google.setOnClickListener(mOnclickListener);
+
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+
+        // 카카오 > 별도의 콜백 객체 선언
         Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
             @Override
             public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
@@ -57,7 +100,6 @@ public class LoginActivity extends AppCompatActivity {
                 return null;
             }
         };
-
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +121,7 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public Unit invoke(Throwable throwable) {
                         updateKakaoLoginUi();
+                        signOut();
                         return null;
                     }
                 });
@@ -91,6 +134,118 @@ public class LoginActivity extends AppCompatActivity {
 
 
     } // onCreate End -------------------------------------------------------------------
+
+    // 구글
+    View.OnClickListener mOnclickListener = new View.OnClickListener() {
+        Intent intent;
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            switch (v.getId()){
+                case R.id.login_google:
+                    signInGoogle();
+                    break;
+                case R.id.login_join_btn: //다른 방법으로 회원가입
+                    intent = new Intent(LoginActivity.this, OtherJoinActivity.class);
+
+                    break;
+
+            }
+        }
+    };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // [START on_start_sign_in]
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+    } // [END on_start_sign_in]
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {// [START onActivityResult]
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {  // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    } // [END onActivityResult]
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {// [START handleSignInResult]
+
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account);
+            // Signed in successfully, show authenticated UI.
+            if (account != null) {//google 인텐트 보내는 값 (유저 데이터)
+                String username = account.getGivenName();
+                String userfamilyname = account.getFamilyName();
+                String useremail = account.getEmail();
+                String userid = account.getId();
+                Uri userphoto = account.getPhotoUrl();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("useremail",useremail);//유저 이메일 주소 넘기기
+                intent.putExtra("userid",userid);//유저 이메일 주소 넘기기
+                startActivity(intent);
+
+
+                btnLogin.setVisibility(View.GONE);
+                login_google.setVisibility(View.GONE);
+                btnLogout.setVisibility(View.VISIBLE);
+            }
+
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+
+        }
+    }
+    // [END handleSignInResult]
+
+    // [START signIn]
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // [END signIn]
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Snackbar.make(findViewById(R.id.layout_main), "Authentication Successed.", Snackbar.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Snackbar.make(findViewById(R.id.layout_main), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) { //update ui code here
+        if (user != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+
     // 화면 touch 시 키보드 숨기기
     public boolean dispatchTouchEvent(MotionEvent ev) {
         View focusView = getCurrentFocus();
@@ -129,17 +284,23 @@ public class LoginActivity extends AppCompatActivity {
 
                     // 버튼
                     btnLogin.setVisibility(View.GONE);
+                    login_google.setVisibility(View.GONE);
                     btnLogout.setVisibility(View.VISIBLE);
                 } else {
                     nickName.setText(null);
                     profileIMG.setImageBitmap(null);
 
+                    login_google.setVisibility(View.VISIBLE);
                     btnLogin.setVisibility(View.VISIBLE);
                     btnLogout.setVisibility(View.GONE);
                 }
                 return null;
             }
         });
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
     }
 
 
