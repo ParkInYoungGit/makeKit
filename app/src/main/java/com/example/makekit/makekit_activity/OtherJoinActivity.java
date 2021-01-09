c644009fa5d4f21942bcca0ec629c06af323edad
young <775973@daum.net>
2021-01-09 10:48:13 +0900

    no message

--- app/src/main/java/com/example/makekit/makekit_activity/OtherJoinActivity.java
+++ app/src/main/java/com/example/makekit/makekit_activity/OtherJoinActivity.java
@@ -6,15 +6,6 @@ import android.os.Bundle;
 
 import com.example.makekit.R;
 
-public class OtherJoinActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_other_join);
-    }
-}
-
 public class OtherJoinActivity {
 }
 
