d352111759ab2309affb56b29ab64f9dcd94e9e5
RiaSong <donna_s@sookmyung.ac.kr>
2021-01-14 17:23:56 +0900

    1/14 _ Ria _ 회원가입 이용약관 / 내용보기 다이얼로그 추가 및 리뷰 쪽 shareVar 넘기는중

--- app/src/main/java/com/example/makekit/makekit_activity/RegisterReviewActivity.java
+++ app/src/main/java/com/example/makekit/makekit_activity/RegisterReviewActivity.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -264,5 +267,23 @@ public class RegisterReviewActivity extends AppCompatActivity {
 //        }
 //    }
 
+    // 화면 touch 시 키보드 숨기기
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View focusView = getCurrentFocus();
+        if (focusView != null) {
+            Rect rect = new Rect();
+            focusView.getGlobalVisibleRect(rect);
+            int x = (int) ev.getX(), y = (int) ev.getY();
+            if (!rect.contains(x, y)) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                if (imm != null)
+                    imm.hideSoftInputFromWindow(focusView.getWindowToken(), 0);
+                focusView.clearFocus();
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
 
-    } // END =========================================================================
+
+
+} // END =========================================================================
