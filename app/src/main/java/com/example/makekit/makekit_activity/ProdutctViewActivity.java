e8b022abe33d0db28647782eb50641fdd6b29d9f
mg6613 <mg6613@naver.com>
2021-01-15 12:39:39 +0900

    Splash Color,

--- app/src/main/java/com/example/makekit/makekit_activity/ProdutctViewActivity.java
+++ app/src/main/java/com/example/makekit/makekit_activity/ProdutctViewActivity.java
@@ -82,7 +82,7 @@ public class ProdutctViewActivity extends AppCompatActivity {
         urlAddrBase = SharVar.urlAddrBase;
         //macIP = "192.168.219.164";
 
-        productNo = "44";
+        productNo = "64";
 //        userEmail = "qkr@naver.com";
 
 //        urlAddrBase = "http://" + macIP + ":8080/makeKit/";
