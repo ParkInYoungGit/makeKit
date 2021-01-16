0b08289afe83e0e8db2e2a6508b63415511b07dd
kyeongmipak <qkrrudal7@naver.com>
2021-01-16 17:32:29 +0900

    1/16_sharvar_kyeongmi

--- app/src/main/java/com/example/makekit/makekit_sharVar/SharVar.java
+++ app/src/main/java/com/example/makekit/makekit_sharVar/SharVar.java
@@ -6,7 +6,7 @@ public class SharVar {
 
 
 
-    public static final String macIP = "192.168.219.164";
+    public static final String macIP = "192.168.2.8";
 
 
     public static final String urlAddrBase = "http://" + macIP + ":8080/makekit/";
