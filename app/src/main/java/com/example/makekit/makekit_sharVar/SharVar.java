a5185035aad3e5c0c8b0d40345edca3ff92530dc
RiaSong <donna_s@sookmyung.ac.kr>
2021-01-18 16:34:34 +0900

    sharvar

--- app/src/main/java/com/example/makekit/makekit_sharVar/SharVar.java
+++ app/src/main/java/com/example/makekit/makekit_sharVar/SharVar.java
@@ -6,10 +6,10 @@ public class SharVar {
 
 
 
-    public static final String macIP = "192.168.2.10";
+    public static final String macIP = "192.168.0.81";
 
 
-    public static final String urlAddrBase = "http://" + macIP + ":8080/makekit/";
+    public static final String urlAddrBase = "http://" + macIP + ":8080/makeKit/";
 
 
 //    public static final String userEmail = "qkr@naver.com";
